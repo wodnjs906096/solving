39b49a36ccac878c30585eba8222c76a51495f6f
lee <wodnjs906096@gmail.com>
2024-06-14 21:42:51 +0900

    fixed

--- boj2805.java
+++ boj2805.java
@@ -4,42 +4,43 @@ import java.io.*;
 public class boj2805 {
   public static void main() throws IOException{
     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());  
+    StringTokenizer st = new StringTokenizer(br.readLine());
 
     int Tree_cnt = Integer.parseInt(st.nextToken());
-    int Need_leg; 
+    int Need_leg = Integer.parseInt(st.nextToken());
+    
     int[] Tree = new int[Tree_cnt];
-
-    if(st.hasMoreTokens())
-      Need_leg = Integer.parseInt(st.nextToken());
-    else 
-      return;
 
     st = new StringTokenizer(br.readLine());
 
-    for(int i = 0; i < Tree_cnt; i++) 
+    for (int i = 0; i < Tree_cnt; i++)
       Tree[i] = Integer.parseInt(st.nextToken());
 
-    int left = 1;
-    int right = Tree[Tree.length - 1] - 1; 
+    Arrays.sort(Tree);
+    
+    int left = 0;
+    int right = Tree[Tree.length - 1];
+    int min_height = 0;
 
-    while(left <= right) {
-      int mid = ( right + left ) / 2;
-      int sum = 0;
-      
-      // 자른 나무 길이 합이 필요 길이보다 많다 = 절단기의 높이를 높여야 한다. 
-      if(sum > Need_leg){ 
-        left = mid+1;
+    while (left <= right) {
+      int mid = (right + left) / 2;
+      long sum = 0;
+
+      for(int i = 0; i < Tree_cnt; i++) {
+        if(Tree[i] > mid)
+        sum += (Tree[i] - mid);
+      }
+      // 자른 나무 길이 합이 필요 길이보다 많다 = 절단기의 높이를 높여야 한다.
+      if (sum >= Need_leg) {
+        left = mid + 1;
+        min_height = mid;
       }
       // 자른 나무 길이 합이 필요 길이보다 적다 = 절단기의 높이를 낮춰야 한다.
-      else if(sum < Need_leg) {
-        right = mid - 1;
-      }
       else {
-        int min_height = mid;
-        System.out.println(min_height);
-        return;
-      }
+        right = mid - 1;
+      } 
     }
+    
+    System.out.println(min_height);
   }
 }
